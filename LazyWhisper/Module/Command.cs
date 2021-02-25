f9354cb685b0b3f478bf9a89138d2de770609f73
AntiqueR <antique.pendulum@gmail.com>
2021-02-26 00:40:12 +0900

    add !add command Method

--- LazyWhisper/Module/Command.cs
+++ LazyWhisper/Module/Command.cs
@@ -10,6 +10,16 @@ namespace LazyWhisper.Module
 {
     public class Command : ModuleBase
     {
-        
+
+        [Command("add")]
+        public async Task AddCommandAsync(params string[] args)
+        {
+            if (args.Length < 2 || args[0].Contains('!'))
+            {
+                await ReplyAsync("!add CommandName Message");
+                return;
+            }
+            var message = args.Skip(1).Aggregate((a, b) => $"{a} {b}");
+        }
     }
 }

294b7500551602f22dccfe0f138c4d4f2a89e0bd
AntiqueR <antique.pendulum@gmail.com>
2021-02-25 18:20:57 +0900

    apply NuGet packages

--- LazyWhisper/Program.cs
+++ LazyWhisper/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using ConsoleAppFramework;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LazyWhisper
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-
+            await Host.CreateDefaultBuilder().ConfigureServices((hostContext, services) =>
+            {
+                services.AddOptions();
+                services.Configure<Config>(hostContext.Configuration);
+            }).RunConsoleAppFrameworkAsync<LazyWhisper>(args);
         }
     }
 }

2bde09487a7982bb689285e98a05a731d8e0f29d
AntiqueR <antique.pendulum@gmail.com>
2021-02-25 22:43:38 +0900

    add IDataRepository Interface

---
+++ LazyWhisper/Module/CustomCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyWhisper.Module
+{
+    public class CustomCommand
+    {
+        public string CommandName { get; set; }
+        public string Reply { get; set; }
+    }
+}
---
+++ LazyWhisper/Module/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyWhisper.Module
+{
+    public interface IDataRepository
+    {
+        Task<CustomCommand> FindAsync(string commandName, ulong channelId);
+        Task<CustomCommand[]> FindAllAsync(ulong channelId);
+        Task DeleteAsync(string commandName, ulong channelId);
+        Task UpdateAsync(string commandName, string reply, ulong channelId);
+    }
+}

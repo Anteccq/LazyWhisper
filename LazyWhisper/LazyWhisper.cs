90e1bd8538937a4c376a25a7828ce617d9b2c2c7
AntiqueR <antique.pendulum@gmail.com>
2021-02-25 23:54:16 +0900

    Preparing to dev Command

--- LazyWhisper/LazyWhisper.cs
+++ LazyWhisper/LazyWhisper.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using ConsoleAppFramework;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 using Microsoft.Extensions.Options;
 
 namespace LazyWhisper
@@ -9,7 +14,40 @@ namespace LazyWhisper
     public class LazyWhisper : ConsoleAppBase
     {
         private IOptions<Config> _config;
+        private DiscordSocketClient _client;
+        private CommandService _command;
 
         public LazyWhisper(IOptions<Config> config) => _config = config;
+
+        public async Task ExecuteAsync()
+        {
+            _client = new DiscordSocketClient();
+            _command = new CommandService();
+            _client.Log += message =>
+            {
+                Console.WriteLine($"{message.Message} : {message.Exception}");
+                return Task.CompletedTask;
+            };
+
+            _client.MessageReceived += MessageHandle;
+            await _command.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            await _client.LoginAsync(TokenType.Bot, _config.Value.Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1, Context.CancellationToken);
+
+            await _client.StopAsync();
+        }
+
+        private async Task MessageHandle(SocketMessage message)
+        {
+            if (!(message is SocketUserMessage msg) || msg.Author.IsBot) return;
+
+            var argPos = 0;
+            if (!(msg.HasCharPrefix('!', ref argPos)) || msg.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
+            var context = new CommandContext(_client, msg);
+            var isSuccess = (await _command.ExecuteAsync(context, argPos, null)).IsSuccess;
+            if(isSuccess) return;
+        }
     }
 }
---
+++ LazyWhisper/Module/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Net;
+
+namespace LazyWhisper.Module
+{
+    public class Command : ModuleBase
+    {
+        
+    }
+}

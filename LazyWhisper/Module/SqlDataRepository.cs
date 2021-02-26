9e44b7975030e855e1b1c0e10780b302fb4f6e2d
AntiqueR <antique.pendulum@gmail.com>
2021-02-27 01:52:21 +0900

    add connection string from DI

--- LazyWhisper/Module/SqlDataRepository.cs
+++ LazyWhisper/Module/SqlDataRepository.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace LazyWhisper.Module
 {
     public class SqlDataRepository : IDataRepository
     {
+        private string _connectionString;
+
+        public SqlDataRepository(IOptions<Config> config)
+        {
+            _connectionString = config.Value.ConnectionStrings;
+        }
+
         public Task<CustomCommand> FindAsync(string commandName, ulong channelId)
         {
             throw new NotImplementedException();
